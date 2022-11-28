cd6025d5839d17e30c59fc35ea080035592653a1
Eli Alejandro <iscelialejandro@gmail.com>
2022-11-28 16:51:36 +0000

    Se agregó la conexión a python

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, jsonify
+import pymongo
 import os
 
 app = Flask(__name__)
@@ -6,6 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
+    try:
+        cliente=pymongo.MongoClient("mongodb+srv://root:s1st3m4s@dbtemp.e6lolhj.mongodb.net/?retryWrites=true&w=majority")
+        db=cliente.ecoSens
+        collection=db.temp
+        print("Colección iniciada")
+        
+        datos=list(collection.find())
+        print(datos)
+        for x in datos:
+            print(x)
+        cliente.close()
+        print("Conexión cerrada")
+
+    except pymongo.errors.ServerSelectionTimeoutError as errorTime:
+        return jsonify({"Choo Choo": "error de conexion1"})
+    except pymongo.errors.ConnectionFailure as errorConexion:
+        return jsonify({"Choo Choo": "error de conexion2"})
+
     return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
 
